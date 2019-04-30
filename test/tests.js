561ad4ecc8cc83236d616235baffa163b8a72486
sepuckett86 <sepuckett86@gmail.com>
2019-04-29 17:40:30 -0700

    tdd test without function

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,19 @@
 const test = QUnit.test;
 
-test('test here', (assert) => {
+test('get form data', (assert) => {
     //Arrange
+    const expected = {
+        name: 'clarinet',
+        price: 2000,
+        description: 'Professional clarinet made in France',
+        family: 'woodwind',
+        accessories: ['reeds', 'neckstrap'],
+        condition: 'new'
+    };
+
     //Act
+    const actual = getProductFormData(formData);
+
     //Assess
+    AbstractRange.equal(actual, expected);
 });
