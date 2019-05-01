0ffbad36f14c19d0b3fa3a4fa0817505dc2b9981
sepuckett86 <sepuckett86@gmail.com>
2019-04-30 17:16:48 -0700

    add slider value display

--- src/entry-form.js
+++ src/entry-form.js
@@ -2,10 +2,18 @@ import getProductFormData from './get-product-form-data.js';
 import instrumentApi from './instrument-api.js';
 
 const form = document.getElementById('instrument-form');
+const slider = document.getElementById('quality');
+const sliderValueText = document.getElementById('slider-value');
+
+sliderValueText.textContent = slider.value;
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const formData = new FormData(form);
     const data = getProductFormData(formData);
     instrumentApi.set(data);
+});
+
+slider.addEventListener('change', () => {
+    sliderValueText.textContent = slider.value;
 });
