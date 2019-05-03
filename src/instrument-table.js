a6600215724fb6c1ac99a5c56f0441eb5f35c363
sepuckett86 <sepuckett86@gmail.com>
2019-05-03 11:14:52 -0700

    add make cell module

--- src/instrument-table.js
+++ src/instrument-table.js
@@ -1,4 +1,5 @@
 import instrumentApi from './instrument-api.js';
+import makeCell from './make-cell.js';
 
 const tableBody = document.getElementById('table-body');
 
@@ -8,35 +9,18 @@ for(let i = 0; i < instruments.length; i++) {
     const instrument = instruments[i];
     const tr = document.createElement('tr');
 
-    const nameCell = makeNameCell(instrument);
+    const nameCell = makeCell.makeNameCell(instrument.name);
+    const priceCell = makeCell.makeTextCell(instrument.price);
+    const familyCell = makeCell.makeTextCell(instrument.family);
+    const conditionCell = makeCell.makeTextCell(instrument.condition);
+    const qualityCell = makeCell.makeTextCell(instrument.quality);
+    
     tr.appendChild(nameCell);
-
-    const priceCell = document.createElement('td');
-    priceCell.textContent = instrument.price;
     tr.appendChild(priceCell);
-
-    const familyCell = document.createElement('td');
-    familyCell.textContent = instrument.family;
     tr.appendChild(familyCell);
-
-    const conditionCell = document.createElement('td');
-    conditionCell.textContent = instrument.condition;
     tr.appendChild(conditionCell);
-
-    const qualityCell = document.createElement('td');
-    qualityCell.textContent = instrument.quality;
     tr.appendChild(qualityCell);
 
     tableBody.appendChild(tr);
 }
 
-function makeNameCell(instrument) {
-    const nameCell = document.createElement('td');
-    const link = document.createElement('a');
-    const searchParams = new URLSearchParams();
-    searchParams.set('name', instrument.name);
-    link.href = './product-detail.html?' + searchParams;
-    link.textContent = instrument.name;
-    nameCell.appendChild(link);
-    return nameCell;
-}
